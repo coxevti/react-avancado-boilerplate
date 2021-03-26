e38197b74931e4209b247428cfd7550c1e0a5ecb
Valdir Coxev [Home] <valdircoxevdev@gmail.com>
2021-03-25 20:41:54 -0400

    26.Configurando o PWA

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,8 +7,10 @@ function App({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <title>React Avançado - Boilerplate</title>
+        <meta name="theme-color" content="#000000" />
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <link rel="manifest" href="/manifest.json" />
         <meta
           name="description"
           content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
