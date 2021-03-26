93d59a41f2ab84b21f84247acbe5eb4b48269421
Valdir Coxev [Home] <valdircoxevdev@gmail.com>
2021-03-25 20:15:32 -0400

    25.Usando Storybook Essentials e Controls

--- src/components/Main/stories.tsx
+++ src/components/Main/stories.tsx
@@ -1,16 +1,9 @@
-import { text, withKnobs } from '@storybook/addon-knobs'
 import { Meta, Story } from '@storybook/react/types-6-0'
 import Main from '.'
 
 export default {
   title: 'Main',
-  component: Main,
-  decorators: [withKnobs]
+  component: Main
 } as Meta
 
-export const Basic: Story = () => (
-  <Main
-    title={text('Title', 'React Avançado')}
-    description={text('Description', 'TypeScript, NextJS e Styled Components')}
-  />
-)
+export const Basic: Story = (args) => <Main {...args} />
